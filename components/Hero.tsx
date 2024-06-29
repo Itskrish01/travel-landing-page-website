fcc471a5ad7f4213b5efb50ced3305e00a544afa
Itskrish01 <krishtasood@gmail.com>
2024-06-29 21:35:46 +0530

    new changes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -61,7 +61,7 @@ const Hero = () => {
       </div>
 
       <div className="relative flex flex-1 items-start">
-        <div className="relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-yellow-600 px-7 py-8">
+        <div className="relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-gray-700 px-7 py-8">
           <div className="flex flex-col">
             <div className="flexBetween">
               <p className="regular-16 text-gray-20">Location</p>
